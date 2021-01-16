9a9313148fb587a03406aa3336a9706bdbec576b
Tomas <tomas.gonzalez@prosegur.com>
2021-01-16 20:04:52 -0300

    hook with mouse effect

--- components/LandingButton.js
+++ components/LandingButton.js
@@ -1,8 +1,12 @@
 import React from "react";
+import useDynamicMousePushing from "../hooks/useDynamicMousePushing";
 
 export default function LandingButton({ name }) {
+    const refElement = useDynamicMousePushing();
+    
 	return (
 		<div
+			ref={refElement}
 			id={name.toLowerCase()}
 			className="big-button flex flex-jc-c flex-ai-c"
 		>
---
+++ hooks/useDynamicMousePushing.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from "react";
+
+export default function useDynamicMousePushing() {
+	const element = useRef(null);
+
+	useEffect(() => {
+		element.current.addEventListener("mousemove", (ev) => {
+			const {
+				offsetLeft,
+				offsetTop,
+				offsetHeight,
+				offsetWidth,
+			} = ev.target;
+			var objCenterX = offsetLeft + offsetWidth / 2;
+			var objCenterY = offsetTop + offsetHeight / 2;
+
+			const x = ev.pageX - objCenterX;
+			const y = ev.pageY - objCenterY;
+			let rotateY = "",
+				rotateX = "";
+			if (x > 0) {
+				rotateY = "rotateY(10deg)";
+				rotateX = y < 0 ? "rotateX(10deg)" : "rotateX(-10deg)";
+			} else {
+				rotateY = "rotateY(-10deg)";
+				rotateX = y < 0 ? "rotateX(10deg)" : "rotateX(-10deg)";
+			}
+			element.current.style.transform = `perspective(600px) ${rotateX} ${rotateY}`;
+		});
+
+		element.current.addEventListener("mouseout", () => {
+			element.current.style.transform = `perspective(600px) rotateX(0) rotateX(0)`;
+		});
+
+		return () => {
+			const resetPosition = () =>
+				(element.current.style.transform = `perspective(600px) rotateX(0) rotateX(0)`);
+			element.current.removeEventListener("mousemove", resetPosition);
+			element.current.removeEventListener("mouseout", resetPosition);
+		};
+	}, []);
+
+	return element;
+}
