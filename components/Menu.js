284f6ebde92464e6a90bd21a3d7c42095aed7fb4
Tomas Gonzalez <tomasg88@gmail.com>
2022-03-11 02:10:55 -0300

    hide menu on change

--- components/Menu.js
+++ components/Menu.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react'
-import Hamburger from './Hamburger'
-import LandingButton from './LandingButton'
+import React, { useState, useEffect } from "react";
+import Hamburger from "./Hamburger";
+import LandingButton from "./LandingButton";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 export default function Menu() {
-	const [isOpen, setIsOpen] = useState(false)
-    return (
-        <div className="menu">
-			<div className="button-container flex">
-				<Hamburger 
-					isOpen={isOpen}
-					setIsOpen={setIsOpen}
-				/>
+	const [isOpen, setIsOpen] = useState(false);
+	const router = useRouter();
+
+	// if menu is open then hide on route change
+	useEffect(() => {
+		setIsOpen(false);
+	}, [router.pathname]);
+
+	return (
+		<motion.div>
+			<div className="menu">
+				<div className="button-container flex">
+					<Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
+				</div>
+
+				<div className={`menu-content ${isOpen ? "open" : ""}`}>
+					<LandingButton name="about" />
+					<LandingButton name="projects" />
+					<LandingButton name="blog" />
+					<LandingButton name="contact" />
+				</div>
 			</div>
-			
-			<div className={`menu-content ${isOpen? 'open' : ''}`}>
-				<LandingButton name="about" />
-				<LandingButton name="projects" />
-				<LandingButton name="blog" />
-				<LandingButton name="contact" />
-			</div>
-        </div>
-    )
+		</motion.div>
+	);
 }
